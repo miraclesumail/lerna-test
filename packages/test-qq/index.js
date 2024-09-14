f483402e75003f926632232a8f9e33458fcf4b88
sumail <sumail@xyzzdev.com>
2024-09-14 21:47:17 +0800

    dd

--- packages/test-qq/index.js
+++ packages/test-qq/index.js
@@ -1,1 +1,9 @@
-console.log('qqqq---wwww')
+/*
+ * @Author: sumail sumail@xyzzdev.com
+ * @Date: 2024-09-14 21:46:14
+ * @LastEditors: sumail sumail@xyzzdev.com
+ * @LastEditTime: 2024-09-14 21:47:00
+ * @FilePath: /lerna-test/packages/test-qq/index.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+console.log('qqqq---wwwdddw')
